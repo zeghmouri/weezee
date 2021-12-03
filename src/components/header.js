bd525ed94b3e7d87c5bf58884517939880750f2a
zeghmouri <hm_zeghmouri@esi.dz>
2021-12-03 17:52:21 +0100

    fin.

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState} from 'react'
 import styles from '../../styles/header.module.css'
 import logo from '../assets/images/logo.svg'
 import icon from '../assets/icons/humburger.svg'
