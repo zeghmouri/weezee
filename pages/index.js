dfcd1dc19186040e74d2133bea010a87ad681c8d
zeghmouri <hm_zeghmouri@esi.dz>
2021-12-08 19:10:33 +0100

    fixing sharing image

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <title>Weezee - WiFi illimité à l&apos;extérieur, à prix cassé !</title>
         <meta name="description" content="Depuis 2014, notre mission est la même : vous connecter facilement aux réseaux WiFis proche de vous grâce à une application complète et novatrice."/>
         <link rel="icon" href="/weezee.svg" />
-        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:image" content={previewImage.src} key="ogimage" />
         <meta property="og:site_name" content="Weezee" key="ogsitename" />
         <meta property="og:title" content="Weezee - WiFi illimité à l&apos;extérieur, à prix cassé !" key="ogtitle" />
         <meta property="og:description" content="Depuis 2014, notre mission est la même : vous connecter facilement aux réseaux WiFis proche de vous grâce à une application complète et novatrice." key="ogdesc" />
